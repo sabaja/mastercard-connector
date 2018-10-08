d9d84d69c3809369c855c1f16685934a2e3271b9
sabaja <jacoposabatini76@gmail.com>
2018-10-08 17:00:48 +0200

    Aggiunta package Stub con enumerazioni

---
+++ src/main/java/com/mastercom/ps/connector/stub/BaseServiceStub.java
@@ -0,0 +1,16 @@
+package com.mastercom.ps.connector.stub;
+
+/**
+ * Questa enumerazione è la rappresentazione dei micro-servizi esposti da
+ * Mastercard
+ * 
+ * @author SabatiniJa
+ *
+ */
+public enum BaseServiceStub {
+	/**
+	 * CaseFiling, Chargebacks, Claims, Fees, Fraud, HealthCheck, Queues,
+	 * Retrievals, Transactions;
+	 */
+	CaseFiling, Chargebacks, Claims, Fees, Fraud, HealthCheck, Queues, Retrievals, Transactions;
+};
